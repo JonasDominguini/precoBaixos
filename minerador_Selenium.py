960c519e983d2718b7597e4ce0ff6f7f03f23d24
Jonas Dominguini De Souza <jonasdominguini@gmail.com>
2022-09-22 12:02:29 -0300

    Giassi monster no minerador Selenium

--- minerador_Selenium.py
+++ minerador_Selenium.py
@@ -45,5 +45,45 @@ class Page:
 
         energetico_Althoff = pd.DataFrame(Althoff_Energetico473, columns=['Produto','Preco'])
         energetico_Althoff.to_csv('althoff/energetico/monsterAlthoff.csv', index=False)
+        sleep(3)
+
+#======================================================================================================#
+# Giassi energetico monster
+class Page1:
+        def __init__(self, driver):
+                self.driver = driver
+
+
+        lista_energetico_Giassi = [] 
+
+        chromedrive_path = 'C:\precoBaixoDesenv\chromedriver.exe'
+        webdriver = webdriver.Chrome(executable_path=chromedrive_path)
+        sleep(3) 
+        webdriver.get('https://www.giassi.com.br/monster?_q=monster&map=ft')
+        sleep(3)        
+
+        content = webdriver.page_source
+        site = bs(content, 'html.parser')  
+      
+        energeticos_Giassi = site.findAll('a', attrs={'class':'vtex-product-summary-2-x-clearLink h-100 flex flex-column'})
+        for energetico_Giassi in energeticos_Giassi:
+                
+                produto_Giassi = energetico_Giassi.find('span', attrs={'class':'vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body'})
+                valo_normal_Giassi = energetico_Giassi.find('p',attrs={'class':"giassi-apps-custom-0-x-priceUnit"})
+
+                produto_final = produto_Giassi.text.strip().rstrip('\n').lstrip('\n')  
+                valor_final = valo_normal_Giassi.text.strip().rstrip('\n').lstrip('\n')
+
+                lista_energetico_Giassi.append([produto_final, 'Valor: ' +  valor_final ])
+        
+        energetico = pd.DataFrame(lista_energetico_Giassi, columns=[ 'Produto','Preco'])
+        energetico.to_csv('giassi/energetico/energeticoLataGiassi.csv', index=False )
+
+        df = pd.read_csv("giassi/energetico/energeticoLataGiassi.csv")
+        Giassi_Energetico473 = df[df.Produto=="Energético Monster Lata 473ml"]
+
+        energetico_Giassi1 = pd.DataFrame(Giassi_Energetico473, columns=['Produto','Preco'])
+        energetico_Giassi1.to_csv('giassi/energetico/monsterGiassi.csv', index=False)
+        webdriver.close()
 
 
