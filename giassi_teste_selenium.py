20994d560ca184ba54678b40d858806f98275333
JONAS DOMINGUINI DE SOUZA <jonasdominguini@gmail.com>
2022-11-05 08:02:48 -0300

    Teste com cerveja selenium althoff

--- giassi_teste_selenium.py
+++ giassi_teste_selenium.py
@@ -5,47 +5,47 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 
 
-class Page:
+class PageAlthoffCerva:
         def __init__(self, driver):
                 self.driver = driver
 
-        lista_vinho_Giassi = [] 
-        
+        lista_cerveja_Althoff = [] 
 
         chromedrive_path = 'C:\precoBaixoDesenv\chromedriver.exe'
         webdriver = webdriver.Chrome(executable_path=chromedrive_path)
 
-        webdriver.get('https://www.giassi.com.br/bebidas-alcoolicas/vinhos?page=6')
-        sleep(3)        
+        webdriver.get('https://emcasa.althoff.com.br/busca/cerveja')
+        sleep(3)
+        cidade = webdriver.find_element(By.CSS_SELECTOR, '#body > div.MuiDialog-root.dynamic-dialog.undefined.MuiModal-root.css-126xj0f > div.MuiDialog-container.MuiDialog-scrollPaper.css-ekeie0 > div > div.dynamic-dialog-content.hidden-scrollbar > div > div.stores-list > div:nth-child(1) > div:nth-child(1) > div.header')
+        button_confirmar = webdriver.find_element(By.CSS_SELECTOR, '#body > div.MuiDialog-root.dynamic-dialog.undefined.MuiModal-root.css-126xj0f > div.MuiDialog-container.MuiDialog-scrollPaper.css-ekeie0 > div > div.modal-bottom-fixed > div > button')
+        cidade.click()
+        sleep(2)
+        button_confirmar.click()
+        sleep(2)
 
         content = webdriver.page_source
-        site = bs(content, 'html.parser')  
-        print('chegou em parsear')
-      
-        vinhos_Giassi = site.findAll('a', attrs={'class':'vtex-product-summary-2-x-clearLink h-100 flex flex-column'})
-        for vinho_Giassi in vinhos_Giassi:
+        site = bs(content, 'html.parser')
+        sleep(1)
+        webdriver.close()
+        cervejas_Althoff = site.findAll('div', attrs={'class':"link-product"})
+        for cerveja_Althoff in cervejas_Althoff:
+                
+                produto_Althoff = cerveja_Althoff.find('span', attrs={'class':'description-text'})
+                valo_normal_Althoff = cerveja_Althoff.find('div',attrs={'class':"price-value"})
 
-                print('chegou em for')                
-                produto_Giassi = vinho_Giassi.find('span', attrs={'class':'vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body'})
-                valor_normal_Giassi = vinho_Giassi.find('p',attrs={'class':"giassi-apps-custom-0-x-priceUnit"})
+                produto_final = produto_Althoff.text.strip().rstrip('\n').lstrip('\n')  
+                valor_final = valo_normal_Althoff.text.strip().rstrip('\n').lstrip('\n')
 
-                print(produto_Giassi)
-                print(valor_normal_Giassi),
-
-                print('chegou em listarr')
-
-                produto_final = produto_Giassi.text.strip().rstrip('\n').lstrip('\n')  
-                valor_final = valor_normal_Giassi.text.strip().rstrip('\n').lstrip('\n')
-
-                lista_vinho_Giassi.append([produto_final, 'Valor: ' +  valor_final ])
+                lista_cerveja_Althoff.append([produto_final, 'Valor R$: ' +  valor_final ])
         
-        vinho = pd.DataFrame(lista_vinho_Giassi, columns=[ 'Produto','Preco'])
-        vinho.to_csv('giassi/vinho/Giassi_vinho_garrafa.csv', index=False )
+        cerveja = pd.DataFrame(lista_cerveja_Althoff, columns=[ 'Produto','Preco'])
+        cerveja.to_csv('althoff/cerveja/cervejaLataAlthoff.csv', index=False )
 
-        df = pd.read_csv("giassi/vinho/vinhoGiassi.csv")
-        Giassi_vinho_garrafa = df[df.Produto=="Vinho Chileno Tinto Meio Seco Concha y Toro Reservado Carménère Valle Central Garrafa 750ml"]
+        df = pd.read_csv("althoff/cerveja/cervejaLataAlthoff.csv")
+        Althoff_cerveja473 = df[df.Produto=="Cerveja Campo Largo Com Vinho Red 500ml"]
 
-        vinho_reservado_Giassi = pd.DataFrame(Giassi_vinho_garrafa, columns=['Produto','Preco'])
-        vinho_reservado_Giassi.to_csv('giassi/vinho/vinho_reservado_Giassi.csv', index=False)
+        cerveja_Althoff = pd.DataFrame(Althoff_cerveja473, columns=['Produto','Preco'])
+        cerveja_Althoff.to_csv('althoff/cerveja/lataAlthoff.csv', index=False)
+        sleep(3)
         webdriver.close()
 
